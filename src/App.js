383856ff9db72463cb8f15900de5b374c1069c74
user <sivtsovatanya1985@gmail.com>
2022-07-13 15:50:16 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUpPage from "./components/SignUpPage";
+
+const App = () => {
+    return (
+        <div>
+            <SignUpPage/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = () => {
+	return (
+		<div>
+			create user
+		</div>
+	)
+}
+
+export default SignUpPage;
